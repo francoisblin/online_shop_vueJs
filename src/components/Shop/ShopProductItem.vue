6a75970dc844afc21b60a85ec18c4f064493a7f4
François Blin <frablin@gmail.com>
2021-01-14 11:24:28 +0100

    Change on ShopProductItem.vue

--- src/components/Shop/ShopProductItem.vue
+++ src/components/Shop/ShopProductItem.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="w-25 border product-container">
-    Item
+  <div class="w-25 p-3 product-container d-flex-column">
+    <div class="d-flex flex-row justify-content-center">
+      <img style="height:150px" :src="product.img" />
+    </div>
+    <div>
+      <h3>{{ product.title }}</h3>
+      <hr>
+    </div>
+    <div>
+      <p>{{ product.description }}</p>
+    </div>
+    <div>
+      <span>{{ product.price }}</span>
+    </div>
+    <button class="btn btn-primary btn-sm float-right">Commander</button>
   </div>
 </template>
 
 <script>
 
 export default {
+  data () {
+    return {
+      product: {
+        img: 'https://www.notebookcheck.biz/uploads/tx_nbc2/air13teaser.jpg',
+        title: 'Mac Book 13',
+        description: 'Notre portable le plus fin et le plus léger est métamorphosé par la puce Apple M1.',
+        price: '1500 €'
+      }
+    }
+  }
 }
 
 </script>
@@ -15,4 +38,9 @@ export default {
 .product-container {
   height: 400px;
 }
+
+p {
+  font-size: 12px;
+}
+
 </style>
